d266e112c24edfc273a81416bead8811c24260ff
user268 <55202360+user268@users.noreply.github.com>
2019-10-26 22:38:37 -0400

    .

---
+++ whats-app01/js/spa.js
@@ -0,0 +1,55 @@
+var app = angular.module('spaApp',['ngRoute']);
+
+app.config(function($routeProvider) {
+$routeProvider.when('/',
+{
+controller:'CustomersController',
+templateUrl: 'app/views/home.html'
+})
+.when('/contact',
+{
+controller: 'BookController',
+templateUrl: 'app/views/contact.html'
+})
+.when('/services',
+{
+controller: 'MenuController',
+templateUrl: 'app/views/services.html'
+})
+.when('/news',
+{
+controller: 'FlightStatusController',
+templateUrl: 'app/views/news.html'
+})
+.when('/works',
+{
+controller: 'ETFinalsController',
+templateUrl: 'app/views/works.html'
+})
+});
+app.controller('BookController', function($scope){
+$scope.customerID = 5;
+});
+app.controller('MenuController', function($scope){
+$scope.customerID = 5;
+});
+app.controller('StoryController', function($scope){
+$scope.customerID = 5;
+});
+app.controller('FlightStatusController', function($scope){
+$scope.customerID = 5;
+});
+app.controller('ETFinalsController', function($scope){
+$scope.customerID = 5;
+});
+app.controller('AdamController', function($scope){
+$scope.customerID = 5;
+});
+app.controller('CustomersController', function($scope) {
+$scope.customers = [
+{"id": 1, "name":"Ted", "total": 5.996},
+{"id": 2, "name":"Michelle", "total": 10.994},
+{"id": 3, "name":"Zed", "total": 10.99},
+{"id": 4, "name":"Tina", "total": 15.994}
+];
+});
