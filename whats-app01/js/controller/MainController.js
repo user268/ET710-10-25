cf5de0ef657e7ca7dd63142be6d1104b252af627
Student <jsitbon@qcc.cuny.edu>
2019-10-30 12:31:26 -0400

    .

---
+++ whats-app01/js/controller/MainController.js
@@ -0,0 +1,6 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'After The Thin Man';
+  $scope.cast = 'William Powell, Myrna Loy';
+  $scope.movieurl = 'https://s3.amazonaws.com/mdlet718/after.m4v';
+  $scope.posterurl = 'https://images-na.ssl-images-amazon.com/images/M/MV5BYWNjMTRkOGMtOGMyZS00NTNlLTg5ZTctYTEzMWM1ZDExZjgzXkEyXkFqcGdeQXVyMDI2NDg0NQ@@._V1_SY1000_CR0,0,664,1000_AL_.jpg';
+}]);
